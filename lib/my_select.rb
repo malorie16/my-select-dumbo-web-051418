fa0d294d17beb6652f99f7252400b06fdea67709
malorie16 <malorie18@gmail.com>
2018-05-05 14:52:55 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,10 @@ def my_select(nums)
  i = 0
  collection = []
  while i < nums.length
-  collection << yield(nums[i])
+   if yield(nums[i])
+     collection << nums[i]
+   end
    i+=1
  end
-
+collection
 end
